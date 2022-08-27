fdd6b2fb285c94e9191dade082954ebc3abb4a52
Travis Steinmetz <travis.steinmetz@yahoo.com>
2022-08-26 22:22:49 -0400

    feat: determining if in a virtual environment or not

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import os
+import urllib.request
 
 running_in_virtualenv = "VIRTUAL_ENV" in os.environ
+print(running_in_virtualenv)
 
 # alternative ways to write this, also supporting the case where
 # the variable is set but contains an empty string to indicate
 # 'not in a virtual environment':
 running_in_virtualenv = bool(os.environ.get("VIRTUAL_ENV"))
-running_in_virtualenv = bool(os.getenv("VIRTUAL_ENV"))
+print(running_in_virtualenv)
 
-import urllib.request
+running_in_virtualenv = bool(os.getenv("VIRTUAL_ENV"))
+print(running_in_virtualenv)
+
+print(os.getenv("VIRTUAL_ENV"))
 
 
 def get_comments() -> None:
     contents = urllib.request.urlopen("https://www.youtube.com/watch?v=BbeAOAmpkg4").read()
-    print(contents)
+    # print(contents)
+
 
 def main():
     get_comments()
